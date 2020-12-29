4d5a350bede0de06a211ec43c3a8d0d7297083cb
Jennifer Funchess <jfunchess@ymail.com>
2020-12-29 17:02:03 -0600

    updated links

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,32 +1,43 @@
+const path = require("path");
 const fs = require("fs");
-const router = require("express").router;
-const store = require("../db/store");
+const { v4: uuidv4 } = require("uuid");
+const db = require("../db/db.json");
+const util = require("util");
+const writeFileSync = util.promisify(fs.writeFile);
+const readFile = fs.readFile;
 
-function getNotes() {
-  let data = fs.readFileSync("Develop/db/db.json", "utf8");
-  let notes = JSON.parse(data);
-  for (let i = 0; i < notes.length; i++) {
-    notes[i].id = "" + i;
-  }
-  return notes;
-}
-
-app.get("/api/notes", function (req, res) {
-  notesData = getNotes();
-  res.json(notesData);
-});
-
-app.post("/api/notes", function (req, res) {
-  notesData.push(req.body);
-  fs.writeFileSync(".Develop/db/db.json", JSON.stringify(notesData), "utf8");
-  res.json(true);
-});
-
-router.delete("/notes/:id", (req, res) => {
-  store
-    .removeNote(req.params.id)
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
-});
-
-module.exports = router;
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    readFile("./db/db.json", (error, data) => {
+      let db = JSON.parse(data);
+      if (error) throw console.log(error);
+      res.json(db);
+    });
+  });
+  app.post("/api/notes", (req, res) => {
+    let note = req.body;
+    let id = uuidv4();
+    note.id = id;
+    db.push(note);
+    writeFileSync("./db/db.json", JSON.stringify(db))
+      .then(() => {
+        res.json(note);
+      })
+      .catch((err) => console.error(err));
+  });
+  app.delete("/api/notes/:id", (req, res) => {
+    readFile("./db/db.json", (error, data) => {
+      let db = JSON.parse(data);
+      let newNote = db.filter((note) => note.id != req.params.id);
+      console.log(newNote);
+      const updateNote = newNote;
+      writeFileSync("./db/db.json", JSON.stringify(updateNote))
+        .then(() => {
+          res.json(updateNote);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  });
+};
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var path = require("path");
 const fs = require("fs");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+
+const app = express();
 
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(`public`));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, function () {
   console.log(`App listening on PORT ${PORT}`);
 });
+
+require("./routes/apiRoute")(app);
+require("./routes/htmlRoutes")(app);
